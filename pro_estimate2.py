ee1d935642f0588b8c1b17b5342e2290379c2d09
axuanwu <1019420880@qq.com>
2016-09-08 16:10:29 +0800

    test

--- pro_estimate2.py
+++ pro_estimate2.py
@@ -24,8 +24,8 @@ class Pro_estimate():
         self.set_array()
 
     def get_pro_r(self, pro_pre, n, m):
-        if n > 100:
-            return 1.0 * n / m  # 抽样m次命中大于一百次 无需优化
+        # if n > 100:
+        #     return 1.0 * n / m  # 抽样m次命中大于一百次 无需优化
         if 0 == m:
             return (pro_pre)
         if (m != 0) and (abs(1.0 * n / m / pro_pre - 1) < 0.05):
@@ -153,5 +153,5 @@ class Pro_estimate():
 
 if __name__ == "__main__":
     b = Pro_estimate()
-    print b.get_pro_r(0.001, 2, 100)
-    print b.get_pro_r(0.000001032, 0, 21432)
+    print b.get_pro_r(0.01, 6, 1000)
+    print b.get_pro_r(0.000001032,8, 21432)
